21266d84aa763680f9875b06ceb75ee4da5c5b4f
Enrique <enriquemg.75@gmail.com>
2022-10-18 07:35:00 +0200

    Laboratorio_17_10
    Ajustes realizados durante las pruebas sobre el robot el 17/10/2022.
    Trayectorias ejecutadas correctamente. Odometria interrumpida al final.
    Se modifica el alacance del try en updateOdometry para evitar que un error detenga el proceso

--- Robot.py
+++ Robot.py
@@ -12,7 +12,7 @@ import math as m
 from multiprocessing import Process, Value, Array, Lock
 
 class Robot:
-  def __init__(self, init_position=[0.0, 0.0, 0.0]):
+  def __init__(self, init_position=[0.0, 0.0, 0.0], logFileName=""):
     """
     Initialize basic robot params. \
 
@@ -46,22 +46,39 @@ class Robot:
       self.encR=self.BP.get_motor_encoder(self.PortR) #Read motor encoder R in degrees
     except IOError as error:
       sys.stdout.write(error)
-
+    
+    # Si se ha especificado un nombre para el fichero se hace log  
+    if logFileName!="":
+      self.LogEnable=True
+      #Configurar fichero para log odometry     
+      print ("El log se realizará en ", logFileName)
+      try:
+        #self.f = open(logFileName, 'a') #mode 'a' append
+        self.f = open(logFileName, 'w') #mode 'w' new
+      except IOError as error:
+        try:
+          self.f = open(logFileName, 'w') #mode 'w' new
+        except IOError as error:
+          sys.stdout.write("No se ha podido crear el fichero para el log: ", logFileName)
+          sys.stdout.write(error)
+          self.LogEnable=False
+    else:
+      self.LogEnable=False
     ##################################################
     # odometry shared memory values
-    self.x = Value('d',0.0)
-    self.y = Value('d',0.0)
-    self.th = Value('d',0.0)
+    self.x = Value('d',init_position[0])
+    self.y = Value('d',init_position[1])
+    self.th = Value('d',init_position[2])
     self.finished = Value('b',1) # boolean to show if odometry updates are finished
 
     # if we want to block several instructions to be run together, we may want to use an explicit Lock
-    #self.lock_odometry = Lock()
+    self.lock_odometry = Lock()
     #self.lock_odometry.acquire()
     #print('hello world', i)
     #self.lock_odometry.release()
 
     # odometry update period
-    self.P = 1.0
+    self.P = 0.1
 
 
 
@@ -71,7 +88,7 @@ class Robot:
         w entra en dps y se convierte inmediatamente a radianes
     """
     print("setting speed to %.2f %.2f" % (v, w))
-    w=m.radians(w)
+    # w=m.radians(w)
     # compute the speed that should be set in each motor ...
     wL=(v-w*self.L/2)/self.R
     wR=(v+w*self.L/2)/self.R
@@ -92,7 +109,31 @@ class Robot:
     w=self.R/self.L*(wR-wL)
     v=self.R/2*(wR+wL)
     return v,w
-
+  def NormPiDegrees(self,Angulo_Grados):
+    #Deja un angulo entre -180 y 180
+    angle= int(Angulo_Grados) % 360 #resto división, puede que no funione si Angulo_Grados no es un entero
+    if angle>180:
+      angle=-360+angle
+    elif angle<-180:
+      angle=360+angle
+    return angle
+  def NormPiRadians(self, Angulo_radianes):
+    #Deja un angulo entre -pi y pi
+    if (Angulo_radianes>2*m.pi):
+      vueltas=m.floor(Angulo_radianes/(2*m.pi))
+      angle=Angulo_radianes-vueltas*2*m.pi
+    elif (Angulo_radianes<-2*m.pi):
+      vueltas=m.floor(Angulo_radianes/(-2*m.pi))
+      angle=Angulo_radianes+vueltas*2*m.pi
+    else:
+      angle=Angulo_radianes
+    #Asegurar que queda entre -pi y pi    
+    if angle>m.pi:
+      angle=-2*m.pi+angle
+    elif angle<-1*m.pi:
+      angle=2*m.pi+angle
+    return angle
+    
   def readOdometry(self):
     """ Returns current value of odometry estimation """
     return self.x.value, self.y.value, self.th.value
@@ -109,67 +150,90 @@ class Robot:
 
 # You may want to pass additional shared variables besides the odometry values and stop flag
   def updateOdometry(self, x_odo, y_odo, th_odo, finished):
-    """ To be filled ...  """
     try:
       # reset encoders
       self.BP.offset_motor_encoder(self.PortL, self.BP.get_motor_encoder(self.PortL)) # reset encoder L
-      self.BP.offset_motor_encoder(self.PortR, self.BP.get_motor_encoder(self.PortR)) # reset encoder R      
+      self.BP.offset_motor_encoder(self.PortR, self.BP.get_motor_encoder(self.PortR)) # reset encoder R
+      encL_prev=self.BP.get_motor_encoder(self.PortL) #Read motor encoder L in degrees
+      encR_prev=self.BP.get_motor_encoder(self.PortR) #Read motor encoder R in degrees      
     except IOError as error:
       #print(error)
       sys.stdout.write(error) 
-      
+    
+    tEnd = time.perf_counter()
+    if self.LogEnable:
+      self.f.writelines("Tiempo; X(mm); Y(mm); Theta(grados)"+"\n")
+    
     while not finished.value:
-      # current processor time in a floating point value, in seconds
-      tIni = time.clock()   
-      ######## UPDATE FROM HERE with your code (following the suggested scheme) ########
-
-      # compute updates
       try:
+        # current processor time in a floating point value, in seconds
+        tIni = time.perf_counter()
+        delta_t=tIni-tEnd
+        ######## UPDATE FROM HERE with your code (following the suggested scheme) ########
+
+        # compute updates
         sys.stdout.write("Reading encoder values .... \n")
-        encL=self.BP.get_motor_encoder(self.PortL) #Read motor encoder L in degrees
-        encR=self.BP.get_motor_encoder(self.PortR) #Read motor encoder R in degrees
-        # reset encoders
-        self.BP.offset_motor_encoder(self.PortL, encL) # reset encoder L
-        self.BP.offset_motor_encoder(self.PortR, encR) # reset encoder R
+        encL_actual=self.BP.get_motor_encoder(self.PortL) #Read motor encoder L in degrees
+        encR_actual=self.BP.get_motor_encoder(self.PortR) #Read motor encoder R in degrees
+          
+
         
+        encL=m.radians(encL_actual-encL_prev)
+        encR=m.radians(encR_actual-encR_prev)
+        encL_prev=encL_actual
+        encR_prev=encR_actual
+        sys.stdout.write("Encoder (%s) increased (in degrees) L: %6d  R: %6d " % (type(encL), encL, encR))
+        
+        
+        #Tracción diferencial
+        wR=encR / delta_t     
+        wL=encL / delta_t       
+        v=self.R*(wR+wL)/2
+        w=self.R*(wR-wL)/self.L
+        delta_theta=w*delta_t
+        
+        self.lock_odometry.acquire()      
+        if self.P<0.5:
+          #Procedimiento aproximado para angulos pequeños
+          delta_s=v*delta_t
+          deltaX=delta_s*m.cos(th_odo.value+delta_theta/2)
+          deltaY=delta_s*m.sin(th_odo.value+delta_theta/2)          
+        else:
+          #Procedimiento exacto. Requiere mayor tiempo de computo
+          if w==0:
+            deltaX=delta_t*v*m.cos(th_odo.value)
+            deltaY=delta_t*v*m.sin(th_odo.value)
+          else:
+            radio=v/w
+            #deltaX=2*radio*m.sin(delta_theta/2)*m.cos(th_odo.value+delta_theta/2)
+            #deltaY=2*radio*m.sin(delta_theta/2)*m.sin(th_odo.value+delta_theta/2)
+            deltaX=radio*(m.sin(th_odo.value+delta_theta)-m.sin(th_odo.value))
+            deltaY=-1*radio*(m.cos(th_odo.value+delta_theta)-m.cos(th_odo.value))
+          
+        # to "lock" a whole set of operations, we can use a "mutex"
+        x_odo.value+=deltaX
+        y_odo.value+=deltaY
+        th_odo.value=self.NormPiRadians(th_odo.value+delta_theta)
+        self.lock_odometry.release()
+
+
+        # save LOG       
+        if self.LogEnable:
+          self.f.writelines(str(tIni) + "; " + str(x_odo.value) + "; " + str(y_odo.value) + "; " + str(m.degrees(th_odo.value)) +"\n")
+        else:
+          sys.stdout.write("Update of odometry ...., X=  %d, Y=  %d, th=  %d \n" %(x_odo.value, y_odo.value, m.degrees(th_odo.value)) )        
+
+        ######## UPDATE UNTIL HERE with your code ########
+
+        tEnd = time.perf_counter()
+        time.sleep(self.P - (tEnd-tIni))
       except IOError as error:
         sys.stdout.write(error)
-      
-      sys.stdout.write("Encoder (%s) increased (in degrees) L: %6d  R: %6d " % (type(encL), encL, encR))     
-
-      delta_t=tIni-tEnd
-      
-      #wR=thetaR / delta_t     
-      #wL=thetaL / delta_t
-      #delta_sR=self.R*encR      
-      #delta_sL=self.R*encL
-      #delta_s=(delta_sR+delta_sL)/2
-      delta_theta=m.radians(encR+encL)/2
-      delta_s=self.R*delta_theta     
-      deltaX=delta_s*m.cos(th_odo+delta_theta/2)
-      deltaY=delta_s*m.sin(th_odo+delta_theta/2)          
- 
-
-      # to "lock" a whole set of operations, we can use a "mutex"
-      self.lock_odometry.acquire()
-      x_odo.value+=deltaX
-      y_odo.value+=deltaY
-      th_odo.value+=delta_theta
-      self.lock_odometry.release()
-
-
-      # save LOG
-      # Need to decide when to store a log with the updated odometry ...
-
-      sys.stdout.write("Update of odometry ...., X=  %d, Y=  %d, th=  %d \n" %(x_odo.value, y_odo.value, th_odo.value) )      
-
-      ######## UPDATE UNTIL HERE with your code ########
-
-      tEnd = time.clock()
-      time.sleep(self.P - (tEnd-tIni))
-
+    ##############################
     #print("Stopping odometry ... X= %d" %(x_odo.value))
-    sys.stdout.write("Stopping odometry ... X=  %.2f, Y=  %.2f, th=  %.2f \n" %(x_odo.value, y_odo.value, th_odo.value))
+    sys.stdout.write("Stopping odometry ... X=  %.2f, Y=  %.2f, th=  %.2f \n" %(x_odo.value, y_odo.value, m.degrees(th_odo.value)))
+    if self.LogEnable:
+      self.f.close
 
 
   # Stop the odometry thread.
--- p2_base.py
+++ p2_base.py
@@ -3,46 +3,181 @@
 import argparse
 import numpy as np
 import time
+import os
 import math as m
 from Robot import Robot
 
+def Trayectoria8(miRobot,v,radio):
+
+  w_modulo=v/radio
+  
+  #Rotar -90º (con v=0) y fijamos (-90º por segundo --> w= angulo / 1s)
+  angulo=m.radians(-90)
+  w=angulo # Para que lo gire en 1 segundo
+  miRobot.setSpeed(0,w)
+  time.sleep(angulo/w)
+  
+  #semicírculo +180º
+  angulo=m.radians(180)
+  w=w_modulo*angulo/abs(angulo) #Darle el signo correcto a w
+  miRobot.setSpeed(v,w)
+  time.sleep(angulo/w) 
+
+  #circulo -360º
+  angulo=m.radians(-360)
+  w=w_modulo*angulo/abs(angulo) #Darle el signo correcto a w
+  miRobot.setSpeed(v,w)
+  time.sleep(angulo/w) 
+  
+  #semicirculo +180º
+  angulo=m.radians(180)
+  w=w_modulo*angulo/abs(angulo) #Darle el signo correcto a w
+  miRobot.setSpeed(v,w)
+  time.sleep(angulo/w) 
+  
+def TrayectoriaLoop(miRobot,v,big_radio,recta,alpha):
+# tan(alpha)=big_radio/(a+b)
+# tan(alpha)=small_radio/a
+# small_radio=a * tan(alpha)
+# a=(big_radio/tan(alpha)) - b
+# siendo b=recta
+
+
+  a=big_radio/m.tan(m.radians(alpha))-recta
+  small_radio=a*m.tan(m.radians(alpha))
+  
+  #Rotar 90º (con v=0) y fijamos (90º por segundo --> w= angulo / 1s)
+  angulo=m.radians(90)
+  w=angulo # Para que lo gire en 1 segundo
+  miRobot.setSpeed(0,w)
+  time.sleep(angulo/w)
+
+  #arco de -90º+alpha
+  angulo=m.radians(-90+alpha)
+  w_modulo=v/small_radio
+  w=w_modulo*angulo/abs(angulo) #Darle el signo correcto a w
+  miRobot.setSpeed(v,w)
+  time.sleep(angulo/w)
+
+  #tramo recto con w=0
+  miRobot.setSpeed(v,0)
+  time.sleep(recta/v)   
+
+  #arco de -180º-2*alpha
+  angulo=m.radians(-180-2*alpha)
+  w_modulo=v/big_radio
+  w=w_modulo*angulo/abs(angulo) #Darle el signo correcto a w
+  miRobot.setSpeed(v,w)
+  time.sleep(angulo/w)
+
+  #tramo recto con w=0
+  miRobot.setSpeed(v,0)
+  time.sleep(recta/v)  
+
+  #arco de -90º+alpha
+  angulo=m.radians(-90+alpha)
+  w_modulo=v/small_radio
+  w=w_modulo*angulo/abs(angulo) #Darle el signo correcto a w
+  miRobot.setSpeed(v,w)
+  time.sleep(angulo/w)
+
+  
+def Girarsobresi(miRobot):
+  w_modulo=v/radio
+  miRobot.setSpeed(0,360/5)
+  time.sleep(5)
+  lectura=miRobot.readSpeed()
+  print(lectura[0], " ,", m.degrees(lectura[1]))        
+  miRobot.setSpeed(0,0)
 
 def main(args):
-  try: 
+  try:
+    if args.type=="8":
+      print("Trayectoria tipo 8")
+      logFileName=os.getcwd() + "/LogT8.txt"
+    elif args.type=="L":
+      print("Trayectoria tipo loop")
+      logFileName=os.getcwd() + "/LogTloop.txt"          
+    else:
+      print("Trayectoria desconocida.")
+      logFileName=""    
     if args.radioD < 0:
       print('d must be a positive value')
       exit(1)
+    if args.alpha <= 0:
+      print('alpha must be a positive value')
+      exit(1)
+    if args.rDist <= 0:
+      print('rDist must be a positive value')
+      exit(1)           
+     
+    if args.file!="":
+      logFileName=os.getcwd() + "/" + args.file
+                
+    
+    # Instantiate Odometry. Default value will be 0,0,0
+    if args.pos_ini:
+      robot = Robot(init_position=args.pos_ini, logFileName=logFileName)
+    else:
+      robot = Robot(logFileName=logFileName)
 
-    robot = Robot()
+    #print("X,Y,Theta value at the beginning from main X= %.2f, Y=%.2f, Theta=%.2f" %(robot.x.value, robot.y.value, robot.th.value))
+    odoValues=robot.readOdometry()
+    print("Odometry at the beginning from main X= %.2f, Y=%.2f, Theta=%.2f" % (odoValues[0],odoValues[1],odoValues[2]))
 
-    print("X value at the beginning from main X= %.2f" %(robot.x.value))
+    # 1. launch updateOdometry Process()
+    robot.startOdometry()
+    tIni=time.perf_counter()
+    print("Start : %s" % tIni )
+    
+    # 2. perform trajectory
+    if args.type=="8":
+      Trayectoria8(robot,args.velo,args.radioD)
+    
+    if args.type=="L":
+      #miRobot,v,big_radio,recta,alpha
+      TrayectoriaLoop(robot,args.velo,args.radioD,args.rDist,args.alpha)          
+    
+    tEnd=time.perf_counter()
+    print("End : %s, Elapsed: %s" % (tEnd, tEnd-tIni) )
 
-    robot.setSpeed(0,360/5)
-    time.sleep(5)
-    lectura=robot.readSpeed()
-    print(lectura[0], " ,", m.degrees(lectura[1]))        
-    robot.setSpeed(0,0)
+
+
+    # 3. wrap up and close stuff ...
+    # This currently unconfigure the sensors, disable the motors,
+    # and restore the LED to the control of the BrickPi3 firmware.
+    robot.stopOdometry()
+
 
   except KeyboardInterrupt:
-  # except the program gets interrupted by Ctrl+C on the keyboard.
-  # THIS IS IMPORTANT if we want that motors STOP when we Ctrl+C ...
+    # except the program gets interrupted by Ctrl+C on the keyboard.
+    # THIS IS IMPORTANT if we want that motors STOP when we Ctrl+C ...
     robot.stopOdometry()
-    f.close()
+
 
 if __name__ == "__main__":
 
   # get and parse arguments passed to main
   # Add as many args as you need ...
   parser = argparse.ArgumentParser()
-  parser.add_argument("-f", "--file", help="filename for log", default="Log.txt")
+  parser.add_argument("-f", "--file", help="filename for log", default="")
+  
   parser.add_argument("-t", "--type", help="Trayectory type, 8(default), L(loop)", default="8")
-  parser.add_argument("-d", "--radioD", help="Radio to perform the 8-trajectory, or loop trajectory big radio (mm)",
-                      type=float, default=400.0)
-  parser.add_argument("-a", "--radioA", help="loop trajectory small radio(mm)",
+  
+  parser.add_argument("-d", "--radioD", help="Radio to perform the 8-trajectory, or big radio for loop trajectory(mm)",
                       type=float, default=200.0)
-  parser.add_argument("-r", "--rDist", help="Distance between centers for loop trajectory (mm)",
-                      type=float, default=800.0)                       
-  parser.add_argument("-p", "--pos_ini", nargs=3, help="Set odometry initial position", type=float)
+                      
+  parser.add_argument("-a", "--alpha", help="loop trajectory tangent angle (degrees)",
+                      type=float, default=10.0)
+                      
+  parser.add_argument("-r", "--rDist", help="Straight section length for loop trajectory (mm)",
+                      type=float, default=480.0)
+                      
+  parser.add_argument("-v", "--velo", help="Linear speed (mm/2)",
+                      type=float, default=100.0)  
+                      
+  parser.add_argument("-p", "--pos_ini", nargs=3, help="Set odometry initial position", type=float)                
   args = parser.parse_args()
-
+  
   main(args)
+  
